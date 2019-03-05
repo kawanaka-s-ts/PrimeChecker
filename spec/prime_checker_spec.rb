66edee35f25df6abf1570b8f0787d4196fea988b
kawanaka <kawanaka_s@tenso.com>
2019-03-05 20:43:13 +0900

    調べたい値の平方根以下の素数でのみ判定するように変更

--- spec/prime_checker_spec.rb
+++ spec/prime_checker_spec.rb
@@ -3,16 +3,17 @@ require "prime_checker"
 
 describe PrimeChecker do
   let(:prime_checker) {PrimeChecker.new}
+
   describe "#check" do
     context "素数を渡した場合" do
       it "trueを返す" do
-        expect(prime_checker.check(2)).to eq true
+        expect(prime_checker.check(241)).to eq true
       end
     end
 
     context "素数ではない数値を渡した場合" do
       it "falseを返す" do
-        expect(prime_checker.check(4)).to eq false
+        expect(prime_checker.check(1080)).to eq false
       end
     end
   end
